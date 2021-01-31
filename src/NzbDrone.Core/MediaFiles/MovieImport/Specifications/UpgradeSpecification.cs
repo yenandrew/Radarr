de59c0612def02d9e6554f8df25af021e1b7bb04
Qstick <qstick@gmail.com>
2021-01-30 23:29:56 -0500

    Fixed: Don't Import Files with lower CF Score

--- src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/UpgradeSpecification.cs
+++ src/NzbDrone.Core/MediaFiles/MovieImport/Specifications/UpgradeSpecification.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using NLog;
+using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download;
 using NzbDrone.Core.Parser.Model;
@@ -8,16 +10,19 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
 {
     public class UpgradeSpecification : IImportDecisionEngineSpecification
     {
+        private readonly ICustomFormatCalculationService _customFormatCalculationService;
         private readonly Logger _logger;
 
-        public UpgradeSpecification(Logger logger)
+        public UpgradeSpecification(ICustomFormatCalculationService customFormatCalculationService, Logger logger)
         {
+            _customFormatCalculationService = customFormatCalculationService;
             _logger = logger;
         }
 
         public Decision IsSatisfiedBy(LocalMovie localMovie, DownloadClientItem downloadClientItem)
         {
             var qualityComparer = new QualityModelComparer(localMovie.Movie.Profile);
+            var preferredWordScore = GetCustomFormatScore(localMovie);
 
             if (localMovie.Movie.MovieFileId > 0)
             {
@@ -34,11 +39,46 @@ namespace NzbDrone.Core.MediaFiles.MovieImport.Specifications
                 if (qualityCompare < 0)
                 {
                     _logger.Debug("This file isn't a quality upgrade for movie. Skipping {0}", localMovie.Path);
-                    return Decision.Reject("Not an upgrade for existing movie file(s)");
+                    return Decision.Reject("Not a quality upgrade for existing movie file(s)");
+                }
+
+                var customFormats = _customFormatCalculationService.ParseCustomFormat(movieFile);
+                var movieFileCustomFormatScore = localMovie.Movie.Profile.CalculateCustomFormatScore(customFormats);
+
+                if (qualityCompare == 0 && preferredWordScore < movieFileCustomFormatScore)
+                {
+                    _logger.Debug("This file isn't a custom format upgrade for movie. Skipping {0}", localMovie.Path);
+                    return Decision.Reject("Not a custom format upgrade for existing movie file(s)");
                 }
             }
 
             return Decision.Accept();
+        }
+
+        private int GetCustomFormatScore(LocalMovie localMovie)
+        {
+            var movie = localMovie.Movie;
+            var scores = new List<int>();
+
+            if (localMovie.FileMovieInfo != null)
+            {
+                var formats = _customFormatCalculationService.ParseCustomFormat(localMovie.FileMovieInfo);
+                scores.Add(movie.Profile.CalculateCustomFormatScore(formats));
+            }
+
+            if (localMovie.FolderMovieInfo != null)
+            {
+                var formats = _customFormatCalculationService.ParseCustomFormat(localMovie.FolderMovieInfo);
+                scores.Add(movie.Profile.CalculateCustomFormatScore(formats));
+            }
+
+            if (localMovie.DownloadClientMovieInfo != null)
+            {
+                var formats = _customFormatCalculationService.ParseCustomFormat(localMovie.DownloadClientMovieInfo);
+                scores.Add(movie.Profile.CalculateCustomFormatScore(formats));
+            }
+
+            return scores.MaxOrDefault();
         }
     }
 }
